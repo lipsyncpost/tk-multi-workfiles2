1fa0ea35350a87d485a9ac61538ca334223a8c6b
Keith <kyfpang@hotmail.com>
2018-11-14 12:58:17 +0000

    restore using the work template when building the unique key for publishes make sure the wp_fields have a name key that contains the value of the pub_name key from publishes

--- python/tk_multi_workfiles/file_finder.py
+++ python/tk_multi_workfiles/file_finder.py
@@ -331,7 +331,7 @@ class FileFinder(QtCore.QObject):
             
             # build the unique file key for the publish path.  All files that share the same key are considered
             # to be different versions of the same file.
-            file_key = FileItem.build_file_key(wp_fields, publish_template, 
+            file_key = FileItem.build_file_key(wp_fields, work_template, 
                                                version_compare_ignore_fields)
             if filter_file_key and file_key != filter_file_key:
                 # we can ignore this file completely!
